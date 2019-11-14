4b10130f276abfa5eb50c11171e1c015e2a3072c
github username <github email address>
2019-11-14 14:45:33 -0800

    got map background onto image

---
+++ src/components/canvas/CanvasBackground.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import backgroundImage from '../../worldpin_canvas_background.png'
+
+const CanvasBackground = () => {
+  return (
+      <img src={backgroundImage} />
+  )
+}
+
+export default CanvasBackground;
---
+++ src/components/canvas/CanvasMap.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CanvasMap = props => {
+  return (
+    <div className="canvas-map">
+      <img src={props.url}>
+    </div>
+  )
+}
+
+export default CanvasTitle;
