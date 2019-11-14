d73cda597bcecad44ab5c564b5568203dd32a94b
github username <github email address>
2019-11-14 14:58:21 -0800

    moved canvas background to css to allow for better styling

--- src/containers/CanvasContainer.js
+++ src/containers/CanvasContainer.js
@@ -1,13 +1,13 @@
 import React, { PureComponent } from 'react'
 import CanvasTitle from '../components/canvas/CanvasTitle.js';
-import CanvasBackground from '../components/canvas/CanvasBackground.js';
+import CanvasMap from '../components/canvas/CanvasMap.js';
 
 class CanvasContainer extends PureComponent {
   render() {
     return (
       <div>
           <CanvasTitle title={"gold sg"} id={1} />
-          <CanvasBackground />
+          <CanvasMap />
       </div>
     )
   }
