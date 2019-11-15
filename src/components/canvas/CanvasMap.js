45b7be1f822c3eb092f7221d95ffba382728b18b
github username <github email address>
2019-11-14 21:51:50 -0800

    added in pincontainer and ready for testing

--- src/components/canvas/CanvasMap.js
+++ src/components/canvas/CanvasMap.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import defaultMap from '../../worldpin_canvas_map.png';
 import background from '../../worldpin_canvas_background.png';
+import PinContainer from '../../containers/PinContainer';
 
 const CanvasMap = (props) => {
   return (
     <div className="canvas-map" style={{ backgroundImage: `url(${props.url || defaultMap}), url(${background})` }}>
+      <PinContainer pins={null} />
     </div>
   )
 }
