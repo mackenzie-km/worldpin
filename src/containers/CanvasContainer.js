ab0ae900544a84df6cc7d3ed64a6eabafbfeee90
Mackenzie Gonzales <github email address>
2019-11-21 10:17:25 -0800

    cleaned up structure of canvas container to include all canvas components and call the pin container from there

--- src/containers/CanvasContainer.js
+++ src/containers/CanvasContainer.js
@@ -1,7 +1,10 @@
 import React, { PureComponent } from 'react'
 import CanvasTitle from '../components/canvas/CanvasTitle.js';
 import CanvasMap from '../components/canvas/CanvasMap.js';
+import CanvasInfo from '../components/canvas/CanvasInfo';
+import PinContainer from './PinContainer';
 
+// All of our canvas components (title, map, info, input) & pin container called & manipulated here 
 class CanvasContainer extends PureComponent {
 
   info = (event) => {
@@ -13,7 +16,9 @@ class CanvasContainer extends PureComponent {
     return (
       <div className="canvas-container">
           <CanvasTitle title={"ten characters"} id={11111} />
-          <CanvasMap url={null} info={this.info} />
+          <CanvasMap url={null} />
+          <CanvasInfo info={this.info} />
+          <PinContainer />
       </div>
     )
   }
