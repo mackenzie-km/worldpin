8f3e7ad86584bbd8f0eb70fa3d915c3704d1e1f1
github username <github email address>
2019-11-20 17:42:52 -0800

    got pin input state to work and styled pin input box

--- src/components/pins/PinInput.js
+++ src/components/pins/PinInput.js
@@ -13,11 +13,13 @@ class PinInput extends PureComponent {
   }
 
   handleColor = (event) => {
-    this.setState({color: event.target.value})
+    let color = event.target.value
+    this.setState({color: color})
   }
 
   handleName = (event) => {
     this.setState({name: event.target.value})
+  console.log(this.state)
   }
 
   handleDescription = (event) => {
@@ -26,23 +28,24 @@ class PinInput extends PureComponent {
 
   render() {
     return (
-      <div className="canvas-input">
+      <div className="pin-input">
         <form onSubmit={event => this.props.handleSubmit(event)}>
-          <label> Name </label>
+          <label> Name </label><br />
             <input type="text" onChange={this.handleName} value={this.state.name} name="name" id="name" /><br />
-          <label> Description </label>
+          <label> Description </label><br />
             <input type="text" onChange={this.handleDescription} value={this.state.description} name="description" id="description" /><br />
-          <label> Color </label>
-            <input type="radio" name="color" value="#61210f" onClick={this.handleColor} />
-            <input type="radio" name="color" value="#e9724c" onClick={this.handleColor} />
-            <input type="radio" name="color" value="#edff1b" onClick={this.handleColor} />
-            <input type="radio" name="color" value="#5dff4e" onClick={this.handleColor} />
-            <input type="radio" name="color" value="#5dff4e" onClick={this.handleColor} />
-            <input type="radio" name="color" value="#5a67fc" onClick={this.handleColor} />
-            <input type="radio" name="color" value="#b63ff3" onClick={this.handleColor} />
-            <input type="radio" name="color" value="#b63ff3" onClick={this.handleColor} />
-            <input type="radio" name="color" value="#000000" onClick={this.handleColor} />
-            <input type="radio" name="color" value="#ffffff" onClick={this.handleColor} />
+          <label> Color </label><br />
+            <input type="radio" name="color" value="#e8028c" id="color-e8028c" onChange={this.handleColor} /><label className="color" htmlFor="color-e8028c" style={{backgroundColor: "#e8028c"}}></label>
+            <input type="radio" name="color" value="#61210f" id="color-61210f" onChange={this.handleColor} /><label className="color" htmlFor="color-61210f" style={{backgroundColor: "#61210f"}}></label>
+            <input type="radio" name="color" value="#e9724c" id="color-e9724c" onChange={this.handleColor} /><label className="color" htmlFor="color-e9724c" style={{backgroundColor: "#e9724c"}}></label>
+            <input type="radio" name="color" value="#edff1b" id="color-edff1b" onChange={this.handleColor} /><label className="color" htmlFor="color-edff1b" style={{backgroundColor: "#edff1b"}}></label>
+            <input type="radio" name="color" value="#5dff4e" id="color-5dff4e" onChange={this.handleColor} /><label className="color" htmlFor="color-5dff4e" style={{backgroundColor: "#5dff4e"}}></label>
+            <input type="radio" name="color" value="#5a67fc" id="color-5a67fc" onChange={this.handleColor} /><label className="color" htmlFor="color-5a67fc" style={{backgroundColor: "#5a67fc"}}></label>
+            <input type="radio" name="color" value="#b63ff3" id="color-b63ff3" onChange={this.handleColor} /><label className="color" htmlFor="color-b63ff3" style={{backgroundColor: "#b63ff3"}}></label>
+            <input type="radio" name="color" value="#000000" id="color-000000" onChange={this.handleColor} /><label className="color" htmlFor="color-000000" style={{backgroundColor: "#000000"}}></label>
+            <input type="radio" name="color" value="#ffffff" id="color-ffffff" onChange={this.handleColor} /><label className="color" htmlFor="color-ffffff" style={{backgroundColor: "#ffffff"}}></label>
+            <br />
+            <input type="submit" />
         </form>
       </div>
     )
--- src/containers/PinContainer.js
+++ src/containers/PinContainer.js
@@ -5,8 +5,13 @@ import PinControls from '../components/pins/PinControls.js';
 class PinContainer extends PureComponent {
 
 addPin = (event) => {
+  console.log(event.type, " before default");
   event.preventDefault();
   console.log("add")
+  setTimeout(function() {
+    console.log(event.type, " during timeout"); // => null
+  }, 2);
+  console.log(event.type, " after timeout")
 }
 
 editPin = (event) => {
@@ -24,11 +29,16 @@ viewPins = (event) => {
   console.log("view")
 }
 
+handleSubmit = (event) => {
+  event.preventDefault()
+  console.log(event)
+}
+
   render() {
     return (
       <div className="pin-container">
           <PinControls addPin={this.addPin} editPin={this.editPin} deletePin={this.deletePin} viewPins={this.viewPins}/>
-          <PinInput id={null} handleSubmit={event=> event.preventDefault()} />
+          <PinInput id={null} handleSubmit={this.handleSubmit} />
       </div>
     )
   }
