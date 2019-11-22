a5b95c7804eb3817dc164cd29b3adda054a29ef9
Mackenzie Gonzales <github email address>
2019-11-21 17:26:36 -0800

    create pin action is complete & set up with cuid

--- src/components/pins/PinControls.js
+++ src/components/pins/PinControls.js
@@ -3,10 +3,10 @@ import React from 'react';
 const PinControls = (props) => {
   return (
     <div className="pin-controls">
-      <button alt="add"><i className="material-icons" onClick={props.addPin}>add_circle</i></button>
-      <button alt="edit"><i className="material-icons" onClick={props.editPin}>edit</i></button>
-      <button alt="cancel"><i className="material-icons" onClick={props.deletePin}>cancel</i></button>
-      <button alt="filter"><i className="material-icons" onClick={props.viewPins}>filter_list</i></button>
+      <button alt="add"><i className="material-icons" onClick={props.addButton}>add_circle</i></button>
+      <button alt="edit"><i className="material-icons" onClick={props.editButton}>edit</i></button>
+      <button alt="cancel"><i className="material-icons" onClick={props.deleteButton}>cancel</i></button>
+      <button alt="filter"><i className="material-icons" onClick={props.viewButton}>filter_list</i></button>
     </div>
   )
 }
--- src/components/pins/PinInput.js
+++ src/components/pins/PinInput.js
@@ -19,7 +19,6 @@ class PinInput extends PureComponent {
 
   handleName = (event) => {
     this.setState({name: event.target.value})
-  console.log(this.state)
   }
 
   handleDescription = (event) => {
@@ -29,7 +28,7 @@ class PinInput extends PureComponent {
   render() {
     return (
       <div className="pin-input">
-        <form onSubmit={event => this.props.handleSubmit(event)}>
+        <form onSubmit={event => this.props.handleSubmit(event, this.state)}>
           <label> Name </label><br />
             <input type="text" onChange={this.handleName} value={this.state.name} name="name" id="name" /><br />
           <label> Description </label><br />
