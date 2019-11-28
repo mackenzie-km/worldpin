48b3b216b16fb5423a71ec10820974afe0d61064
github username <github email address>
2019-11-28 11:16:06 -0800

    stubbed in color filter. ready to add color dialogue.

--- src/reducers/filterReducer.js
+++ src/reducers/filterReducer.js
@@ -1,10 +1,11 @@
-import { VisibilityFilters } from '../actions'
-
- const filterReducer = (state = 'VisibilityFilters.SHOW_ALL', action = null) => {
-debugger
+const filterReducer = (state = 'SHOW_ALL', action = {type: 'SHOW_ALL', criteria: null}) => {
   switch (action.type) {
-    case 'SET_VISIBILITY_FILTER':
-      return action.filter
+    case 'SHOW_ALL':
+      return {type: 'SHOW_ALL', criteria: null}
+    case 'SHOW_PIN_BY_ID':
+      return {type: 'SHOW_PIN_BY_ID', criteria: action.criteria}
+    case 'SHOW_PINS_BY_COLOR':
+      return {type: 'SHOW_PINS_BY_COLOR', criteria: action.criteria}
     default:
       return state
   }
