bc1a7427d8099f25c80999269728d73320b373c2
github username <github email address>
2019-11-29 16:08:59 -0800

    created basic map controller structure to render json

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,22 +1,31 @@
 class MapController < ApplicationController
-  def index
-  end
-
-  def new
-  end
-
+  before_action :find_map, only: [:show, :edit, :update, :destroy]
   def create
+    @map = Map.new(map_params)
+    render json: @map.to_json 
   end
 
   def show
-  end 
-
-  def edit
+    render json: @map.to_json
   end
 
   def update
+    @map.update(map_params)
+    render json: @map.to_json
   end
 
   def destroy
+    @map.delete_all
+  end
+
+  private
+
+  def map_params
+    params.require(:map).permit(:id, :title, :url, :pins)
+  end
+
+  def find_map(id)
+    @map = Map.find(map_params[:id])
+    return @map
   end
 end
