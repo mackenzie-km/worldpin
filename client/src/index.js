66d442eaaabb97ce95db5b81b46db782b50c6cd0
Mackenzie Gonzales <github email address>
2019-12-01 12:53:30 -0800

    added in thunk & compose to handle fetch requests

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(
+  rootReducer, compose(
+    applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+// to apply multiple reducer enhancers, need compose
 
 ReactDOM.render(
   <Provider store={store}>
