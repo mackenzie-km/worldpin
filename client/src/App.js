c95916bbad891bad38a5c9190b8fe4a1a9cf66f6
Mackenzie Gonzales <github email address>
2019-11-30 22:12:55 -0800

    fixed file structure to match client within api main directory

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CanvasContainer from './containers/CanvasContainer.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CanvasContainer />
+    </div>
+  );
+}
+
+export default App;
