9a8692e18193789e5df035e80c5460adfebbea35
github username <github email address>
2019-12-01 13:56:10 -0800

    added in code for link router import

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import CanvasContainer from './containers/CanvasContainer.js';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
+const App = (props) => {
   return (
-    <div className="App">
-      <CanvasContainer />
-    </div>
+    <Router>
+      {<div className="App">
+        <Switch>
+          <Route path="/">
+            <CanvasContainer />
+          </Route>
+        </Switch>
+      </div>}
+    </Router>
   );
 }
 
