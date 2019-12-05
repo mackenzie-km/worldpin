338d71ed5cc787ef9f6ddace07e884648b9f4449
github username <github email address>
2019-12-05 12:30:29 -0800

    added loading map and load pins methods with appropriate state return values

--- client/src/reducers/pinReducer.js
+++ client/src/reducers/pinReducer.js
@@ -1,12 +1,16 @@
 import cuid from 'cuid';
 function pinReducer (state = [], action) {
   switch (action.type) {
+    case "LOADING_MAP":
+      return [...state]
+    case 'LOAD_PINS':
+      return action.json.pins
     case 'CREATE_PIN':
-      return [...state, { id: cuid(), name: action.data.name, location: action.data.location, description: action.data.description, color: action.data.color }];
+      return [...state, { id: cuid(), name: action.data.name, x: action.data.x, y: action.data.y, description: action.data.description, color: action.data.color }];
     case 'DELETE_PIN':
       return [...state.filter(x=> x.id !== action.data)];
     case 'EDIT_PIN':
-      return [...state.filter(x=> x.id !== action.data.id), { id: action.data.id, name: action.data.name, location: action.data.location, description: action.data.description, color: action.data.color }];
+      return [...state.filter(x=> x.id !== action.data.id), { id: action.data.id, name: action.data.name, x: action.data.x, y: action.data.y, description: action.data.description, color: action.data.color }];
     default:
       return [...state];
   }
