740f095f0d49423130c11d05387194bd499d5f0d
Mackenzie Gonzales <github email address>
2019-12-05 13:26:49 -0800

    updated reducer structure to separate pin and loading status for future ajax calls

--- client/src/reducers/pinReducer.js
+++ client/src/reducers/pinReducer.js
@@ -1,18 +1,20 @@
 import cuid from 'cuid';
 function pinReducer (state = [], action) {
   switch (action.type) {
-    case "LOADING_MAP":
-      return [...state]
+    case 'LOADING_MAP':
+      return {pins: [...state.pins], loading: true}
     case 'LOAD_PINS':
-      return action.json.pins
+      return {pins: action.json.pins, loading: false}
+    case 'CREATING_PIN':
+      return {pins: [...state.pins], loading: true}
     case 'CREATE_PIN':
-      return [...state, { id: cuid(), name: action.data.name, x: action.data.x, y: action.data.y, description: action.data.description, color: action.data.color }];
+      return {pins: [...state.pins, { id: cuid(), name: action.data.name, x: action.data.x, y: action.data.y, description: action.data.description, color: action.data.color }], loading: false};
     case 'DELETE_PIN':
-      return [...state.filter(x=> x.id !== action.data)];
+      return {pins: [...state.pins.filter(x=> x.id !== action.data)], loading: false};
     case 'EDIT_PIN':
-      return [...state.filter(x=> x.id !== action.data.id), { id: action.data.id, name: action.data.name, x: action.data.x, y: action.data.y, description: action.data.description, color: action.data.color }];
+      return {pins: [...state.pins.filter(x=> x.id !== action.data.id), { id: action.data.id, name: action.data.name, x: action.data.x, y: action.data.y, description: action.data.description, color: action.data.color }], loading: false};
     default:
-      return [...state];
+      return {pins: [...state], loading: false};
   }
 }
 
