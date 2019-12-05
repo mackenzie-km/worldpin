23b05f800621708701346b15f0b5e6694859edd8
Mackenzie Gonzales <github email address>
2019-12-05 12:29:11 -0800

    added in state and dispatch methods & component did mount methods so that fetch call can update store. also, fixed capturedClick to deal with x & y instead of location.

--- client/src/containers/CanvasContainer.js
+++ client/src/containers/CanvasContainer.js
@@ -2,23 +2,24 @@ import React, { PureComponent } from 'react'
 import CanvasTitle from '../components/canvas/CanvasTitle.js';
 import CanvasMap from '../components/canvas/CanvasMap.js';
 import CanvasInfo from '../components/canvas/CanvasInfo';
+import { connect } from 'react-redux'
 import PinContainer from './PinContainer';
 
 // All of our canvas components (title, map, info, input) & pin container called & manipulated here
 class CanvasContainer extends PureComponent {
 
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
     this.state = {
+      canvasId: props.id,
       canvasInfo: false,
       capturedClick: [],
       browserSize: {x: 1366, y: 768}
     }
   }
 
-  componentDidMount(){
-    // this.calculateOffset()
-    // window.addEventListener("resize", this.calculateOffset);
+  componentDidMount() {
+    this.props.fetchMapInfo(this.state.canvasId)
   }
 
   toggleInfo = (event) => {
@@ -47,10 +48,39 @@ class CanvasContainer extends PureComponent {
           <CanvasMap url={null} handleMapClick={this.handleMapClick} />
           <button id="canvas-info" onClick={this.toggleInfo} alt="info"><i className="material-icons">info</i></button>
           {!!this.state.canvasInfo ? <CanvasInfo /> : null }
-          <PinContainer capturedClick={this.state.capturedClick} browserSize={this.state.browserSize} />
+          <PinContainer id={this.state.canvasId} capturedClick={this.state.capturedClick} browserSize={this.state.browserSize} />
       </div>
     )
   }
 }
 
-export default CanvasContainer;
+
+const getVisiblePins = (pins, data) => {
+  switch (data.type) {
+    case 'SHOW_ALL':
+      return pins;
+    case 'SHOW_PIN_BY_ID':
+      return pins.filter(x => x.id === data.criteria);
+    case 'SHOW_PINS_BY_COLOR':
+      return pins.filter(x => x.color === data.criteria);
+    default:
+      return pins
+  }
+}
+
+const mapStateToProps = (state) => {
+  return { pins: getVisiblePins(state.pinReducer, state.filterReducer) }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchMapInfo: (id) => {
+      dispatch ({type: 'LOADING_MAP'});
+      fetch(`/maps/${id}`)
+      .then(res => res.json())
+      .then(json => dispatch({type: 'LOAD_PINS', json}))
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(CanvasContainer);
