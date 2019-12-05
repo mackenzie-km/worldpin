222c744359ef841c7929493f27bd77770c80146f
github username <github email address>
2019-12-05 12:32:11 -0800

    updating this to use match params as canvas Id

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
-import CanvasContainer from './containers/CanvasContainer.js';
 import './App.css';
+import CanvasContainer from './containers/CanvasContainer.js';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import fetchPins from './actions/api.js';
+// import pinInput from './components/pins/PinInput.js';
 
 const App = (props) => {
   return (
     <Router>
       {<div className="App">
-        <Switch>
-          <Route path="/">
-            <CanvasContainer />
-          </Route>
-        </Switch>
+          <Route path="/maps/:id" render={(props)=> <CanvasContainer id={props.match.params.id} />} />
       </div>}
     </Router>
   );
 }
 
 export default App;
+
+// <Switch>
+//   //
+//   // <Route path="/maps/:id" render={(props) => fetchMapInfo(props) } />
+//   // <Route path="/maps/:id/new" component={pinInput} />
+// </Switch>
