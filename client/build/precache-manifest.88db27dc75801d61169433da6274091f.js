a7a922919b306916928e2851202f2749d847022a
github username <github email address>
2019-12-14 22:04:45 -0800

    setting up production

---
+++ client/build/precache-manifest.88db27dc75801d61169433da6274091f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beea1d861881c4201691125826628429",
+    "url": "/index.html"
+  },
+  {
+    "revision": "242cdc807a2d64df673e",
+    "url": "/static/css/main.ea8b7841.chunk.css"
+  },
+  {
+    "revision": "b0cb634d9b5ca756032a",
+    "url": "/static/js/2.25484455.chunk.js"
+  },
+  {
+    "revision": "242cdc807a2d64df673e",
+    "url": "/static/js/main.3207ec5d.chunk.js"
+  },
+  {
+    "revision": "5413d689a5f60415e06b",
+    "url": "/static/js/runtime-main.3c392af6.js"
+  },
+  {
+    "revision": "278f1c9b9c44240b7c079c41f4c50ae2",
+    "url": "/static/media/worldpin_canvas_map.278f1c9b.png"
+  }
+]);
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
   resources :maps do
     resources :pins, except: [:edit, :new]
   end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
